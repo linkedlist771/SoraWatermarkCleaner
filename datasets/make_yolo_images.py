f25e7532e0da9a600e083045411484003bd708b7
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-10-08 23:19:37 +0800

    Update make_yolo_images.py

--- datasets/make_yolo_images.py
+++ datasets/make_yolo_images.py
@@ -2,18 +2,23 @@ from pathlib import Path
 
 import cv2
 from tqdm import tqdm
-
+from sorawm.watermark_detector import SoraWaterMarkDetector
 from sorawm.configs import ROOT
 
 videos_dir = ROOT / "videos"
 datasets_dir = ROOT / "datasets"
 images_dir = datasets_dir / "images"
 images_dir.mkdir(exist_ok=True, parents=True)
+detector = SoraWaterMarkDetector()
+
 
 if __name__ == "__main__":
     fps_save_interval = 1  # Save every 1th frame
 
-    idx = 0
+    video_idx = 0
+    image_idx = 0  # 全局图片索引
+    total_failed = 0  # 检测失败的总数
+    
     for video_path in tqdm(list(videos_dir.rglob("*.mp4"))):
         # Open the video file
         cap = cv2.VideoCapture(str(video_path))
@@ -24,26 +29,32 @@ if __name__ == "__main__":
 
         frame_count = 0
 
-        while True:
-            ret, frame = cap.read()
+        try:
+            while True:
+                ret, frame = cap.read()
 
-            # Break if no more frames
-            if not ret:
-                break
+                # Break if no more frames
+                if not ret:
+                    break
 
-            # Save frame at the specified interval
-            if frame_count % fps_save_interval == 0:
-                # Create filename: image_idx_framecount.jpg
-                image_filename = f"image_{idx:06d}_frame_{frame_count:06d}.jpg"
-                image_path = images_dir / image_filename
+                # Save frame at the specified interval
+                if frame_count % fps_save_interval == 0:
+                    if not detector.detect(frame)["detected"]:
+                        # Create filename: image_idx_framecount.jpg
+                        image_filename = f"failed_image_{image_idx:06d}_frame_{frame_count:06d}.jpg"
+                        image_path = images_dir / image_filename
 
-                # Save the frame
-                cv2.imwrite(str(image_path), frame)
+                        # Save the frame
+                        cv2.imwrite(str(image_path), frame)
+                        image_idx += 1
+                        total_failed += 1
 
-            frame_count += 1
+                frame_count += 1
 
-        # Release the video capture object
-        cap.release()
-        idx += 1
+        finally:
+            # Release the video capture object
+            cap.release()
+        
+        video_idx += 1
 
-    print(f"Processed {idx} videos, extracted frames saved to {images_dir}")
+    print(f"Processed {video_idx} videos, extracted {total_failed} failed detection frames to {images_dir}")
